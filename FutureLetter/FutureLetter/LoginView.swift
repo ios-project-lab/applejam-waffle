0f9b67856fd066d517725d7517b03dc716b070dd
younnii <132765712+seojungyoun@users.noreply.github.com>
2025-10-27 14:12:24 +0900

    로그인, 회원가입 링크

--- FutureLetter/FutureLetter/LoginView.swift
+++ FutureLetter/FutureLetter/LoginView.swift
@@ -9,7 +9,16 @@ import SwiftUI
 
 struct LoginView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView{
+            VStack{
+                NavigationLink(destination: HomeView()){
+                    Text("로그인")
+                }
+                NavigationLink(destination: SignUpView()){
+                    Text("회원가입")
+                }
+            }
+        }
     }
 }
 
