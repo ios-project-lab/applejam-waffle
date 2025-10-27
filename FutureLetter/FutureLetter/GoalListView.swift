2952bd306a519dff2268992a068409bec51abefe
kcw9609 <kang960953@gmail.com>
2025-10-27 14:17:26 +0900

    홈 화면 네비게이션 링크 연결

---
+++ FutureLetter/FutureLetter/GoalListView.swift
@@ -0,0 +1,18 @@
+//
+//  GoalListView.swift
+//  FutureLetter
+//
+//  Created by mac08 on 10/27/25.
+//
+
+import SwiftUI
+
+struct GoalListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GoalListView()
+}
--- FutureLetter/FutureLetter/HomeView.swift
+++ FutureLetter/FutureLetter/HomeView.swift
@@ -10,6 +10,24 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            VStack {
+                Text("홈")
+                    .font(.title)
+                    .padding()
+                Text("홈 페이지")
+                List {
+                    NavigationLink("정보 수정", destination: ProfileUpdateView())
+                    NavigationLink("편지 쓰기", destination: LetterComposeView())
+                    NavigationLink("편지 열기", destination: LetterDetailView()) // 최근 도착한 편지
+                    NavigationLink("목표 작성하기", destination: SetGoalView())
+                    NavigationLink("목표 관리하기", destination: GoalListView())
+                    
+                }
+                .navigationTitle("친구 목록")
+            }
+        }
+        .navigationTitle("친구")
     }
 }
 
