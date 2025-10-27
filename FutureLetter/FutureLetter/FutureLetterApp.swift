da2eb8c123a14d6ec9ce228260eb059c34bd8eea
kcw9609 <kang960953@gmail.com>
2025-10-27 12:31:57 +0900

    first commit

---
+++ FutureLetter/FutureLetter/FutureLetterApp.swift
@@ -0,0 +1,17 @@
+//
+//  FutureLetterApp.swift
+//  FutureLetter
+//
+//  Created by mac08 on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct FutureLetterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
