5e32351dcb6e63ffeeffcd4da5f18d19ef449aaf
kcw9609 <kang960953@gmail.com>
2025-11-03 14:28:59 +0900

    signup update

--- FutureLetter/FutureLetter/SignUpView.swift
+++ FutureLetter/FutureLetter/SignUpView.swift
@@ -14,6 +14,10 @@ struct SignUpView: View {
     @State private var id = ""
     @State private var pwd = ""
     @State private var displayName = ""
+    @State private var birthday = Date()
+    @State private var gender = "모름"
+
+    let genders = ["남성", "여성", "모름"]
 
     var body: some View {
         VStack(spacing: 12) {
@@ -23,8 +27,17 @@ struct SignUpView: View {
             TextField("ID", text: $id).textFieldStyle(.roundedBorder)
             SecureField("비밀번호", text: $pwd).textFieldStyle(.roundedBorder)
             TextField("닉네임", text: $displayName).textFieldStyle(.roundedBorder)
+            DatePicker("생년월일", selection: $birthday, displayedComponents: .date)
+                            .datePickerStyle(.compact)
+                            .labelsHidden()
+                        
+                        Picker("성별", selection: $gender) {
+                            ForEach(genders, id: \.self) { g in
+                                Text(g)
+                            }
+                        }
             Button {
-                appState.currentUser = User(username: id, displayName: displayName.isEmpty ? id : displayName)
+                appState.currentUser = User(username: id, displayName: displayName.isEmpty ? id : displayName, birthday, gender)
                 presentationMode.wrappedValue.dismiss()
             } label: {
                 Text("가입하기")
