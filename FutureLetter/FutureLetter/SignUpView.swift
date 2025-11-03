9de02339266ad181276a7a68b4847eda5b26f89c
kcw9609 <kang960953@gmail.com>
2025-11-03 16:42:45 +0900

    회원가입 기능 구현

--- FutureLetter/FutureLetter/SignUpView.swift
+++ FutureLetter/FutureLetter/SignUpView.swift
@@ -1,10 +1,3 @@
-//
-//  SignUpView.swift
-//  FutureLetter
-//
-//  Created by Chaemin Yu on 10/27/25.
-//
-
 import SwiftUI
 
 struct SignUpView: View {
@@ -24,21 +17,29 @@ struct SignUpView: View {
             Text("회원가입")
                 .font(.title2)
                 .foregroundColor(.white)
-            TextField("ID", text: $id).textFieldStyle(.roundedBorder)
-            SecureField("비밀번호", text: $pwd).textFieldStyle(.roundedBorder)
-            TextField("닉네임", text: $displayName).textFieldStyle(.roundedBorder)
+            
+            TextField("ID", text: $id)
+                .textFieldStyle(.roundedBorder)
+            
+            SecureField("비밀번호", text: $pwd)
+                .textFieldStyle(.roundedBorder)
+            
+            TextField("닉네임", text: $displayName)
+                .textFieldStyle(.roundedBorder)
+            
             DatePicker("생년월일", selection: $birthday, displayedComponents: .date)
-                            .datePickerStyle(.compact)
-                            .labelsHidden()
-                        
-                        Picker("성별", selection: $gender) {
-                            ForEach(genders, id: \.self) { g in
-                                Text(g)
-                            }
-                        }
+                .datePickerStyle(.compact)
+                .labelsHidden()
+            
+            Picker("성별", selection: $gender) {
+                ForEach(genders, id: \.self) { g in
+                    Text(g)
+                }
+            }
+            .pickerStyle(SegmentedPickerStyle())
+            
             Button {
-                appState.currentUser = User(username: id, displayName: displayName.isEmpty ? id : displayName, birthday, gender)
-                presentationMode.wrappedValue.dismiss()
+                signUp()
             } label: {
                 Text("가입하기")
                     .frame(maxWidth: .infinity)
@@ -47,10 +48,42 @@ struct SignUpView: View {
                     .cornerRadius(8)
                     .foregroundColor(.white)
             }
+            
             Spacer()
         }
         .padding()
         .background(Color("NavyBackground").edgesIgnoringSafeArea(.all))
         .navigationTitle("회원가입")
+    }
+
+    func signUp() {
+        // 앱 상태에 저장
+        appState.currentUser = User(username: id, displayName: displayName.isEmpty ? id : displayName)
+        
+        // PHP 서버 연동
+        guard let url = URL(string: "http://localhost:80/fletter/signup.php") else { return }
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        let birthdayString = dateFormatter.string(from: birthday)
+        
+        let body = "id=\(id)&pwd=\(pwd)&displayName=\(displayName)&birthday=\(birthdayString)&gender=\(gender)"
+        request.httpBody = body.data(using: .utf8)
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let error = error {
+                print("회원가입 에러:", error)
+                return
+            }
+            if let data = data, let responseString = String(data: data, encoding: .utf8) {
+                print("서버 응답:", responseString)
+            }
+        }.resume()
+        
+        presentationMode.wrappedValue.dismiss()
     }
 }
