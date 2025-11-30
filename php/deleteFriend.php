5fab781b30ac2e6de43fa417daf85e3fe0956f07
Chaewon Kang <101781313+kcw9609@users.noreply.github.com>
2025-12-01 01:14:10 +0900

    Add files via upload

---
+++ php/deleteFriend.php
@@ -0,0 +1,35 @@
+<?php
+// DB 접속 정보 (환경 변수 사용)
+include_once('./config.php');
+
+// 응답 헤더 설정
+header('Content-Type: application/json');
+
+// 데이터베이스 연결
+$conn = new mysqli($host, $user, $pw, $dbName);
+
+if ($conn->connect_error) {
+    echo json_encode(["error" => "Database Connection failed: " . $conn->connect_error]);
+    exit();
+}
+$friendsId = $_POST['friendsId'];
+
+if (!$friendsId) {
+    echo "missing_params";
+    exit;
+}
+
+$query = "
+UPDATE Friends
+SET friendStatus = 'deleted', updatedAt = NOW()
+WHERE friendsId = '$friendsId' AND friendStatus = 'accepted'
+";
+
+if (mysqli_query($conn, $query)) {
+    echo "success";
+} else {
+    echo "fail";
+}
+$stmt->close();
+$conn->close();
+?>
